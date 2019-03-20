a71130d4ce1c67ace75a0ed42a0a0ea47c484a4e
hsauers5 <hsauers5@gmail.com>
2019-03-19 23:07:07 -0400

    Implemented server provisioning.

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
+from botocore.exceptions import ClientError
+
 from creds import Creds
 import boto3
 
 
-credentials = Creds("credentials.csv")
+creds = Creds("credentials.csv")
 
+REGION = "us-east-2"
+ec2 = boto3.client(
+    'ec2',
+    aws_access_key_id=creds.access_key_id,
+    aws_secret_access_key=creds.secret_key,
+    region_name=REGION
+)
 
-print("Hello python")
+# Ubuntu Server 18.04 ID from the AWS panel
+image_id = "ami-0f65671a86f061fcd"
+
+# Second smallest instance, free tier eligible.
+instance_type = "t2.micro"
+
+# Make this a command-line argument in the future.
+keypair_name = "robot"
+
+response = {}
+try:
+    response = ec2.run_instances(ImageId=image_id,
+                                 InstanceType=instance_type,
+                                 KeyName=keypair_name,
+                                 MinCount=1,
+                                 MaxCount=1)
+
+    print(response['Instances'][0])
+
+except ClientError as e:
+        print(e)
+
+response = ec2.describe_instances()
+print(response)
